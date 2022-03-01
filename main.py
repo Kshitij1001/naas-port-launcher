c711db9769b50104066094695c21f42426d23454
Kshitij1001 <kshitij4078@gmail.com>
2022-03-01 14:33:22 +0530

    better handling of IAP response

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ def get_docs():
 
 @app.route('/auth')
 def get_api():
-    cred = request.args.to_dict()   #recieved credentials
+    print('json recieved from swagger>>', cred := request.args.to_dict())   #recieved credentials
     SNDBX = cred.pop('sandboxNumber')
+    print('json sent to IAP login>>', toIAP := {'user': cred})
 
-    print(toIAP := {'user': cred})
-
-    login_resp = requests.post(f'http://titansdevcarrier.clcloud.af.qwest.net:40{SNDBX}/login',json=toIAP)
-
-    if login_resp.status_code == 200:
-        return Response(login_resp.text, status=200)
-
-    elif login_resp.status_code == 500:   #can be MongoServerSelectionError
-        return Response(login_resp.json()['error'],status=400)  #will return error message
-    else:
-        return Response("Can't reach the IAP server",status=400)
+    try:
+        login_resp = requests.post(f'http://titansdevcarrier.clcloud.af.qwest.net:40{SNDBX}/login',json=toIAP)
+        
+        if login_resp.status_code == 200:
+            return Response(login_resp.text, status=200)
+        elif login_resp.status_code == 500:   #can be MongoServerSelectionError or invalid username/password
+            return Response(login_resp.json()['error'],status=400)  #will return error message
+    except:
+        pass
+    return Response("Can't reach the IAP server",status=400)
 
 app.run(use_reloader=True, debug=False)
