70ee3b1f384f163b1a296bb5267cad95c57745d3
Kshitij1001 <kshitij4078@gmail.com>
2022-03-03 01:35:48 +0530

    added an API to execute a workflow

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 from flask import Flask, Response, render_template, request
-import requests
+import json
+from sandbox_apis import get_token_response, execute_wf
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def root():
-    print('sending root')
-    return render_template('index.html')
+    print("sending root")
+    return render_template("index.html")
 
-@app.route('/docs')
+
+@app.route("/docs")
 def swagger_docs():
-    print('sending swagger docs')
-    return render_template('swaggerui.html')
+    print("sending swagger docs")
+    return render_template("swaggerui.html")
 
-@app.route('/auth')
+
+@app.route("/auth")
 def login():
-    
-    print('json recieved from swagger>>',data_from_swagger := request.args.to_dict())   #recieved credentials
-    sandboxNum = data_from_swagger.pop('sandboxNumber')
-    print('json data for IAP login>>', data_to_IAP := {'user': data_from_swagger})
+    print(
+        "json recieved from swagger>>", data_from_swagger := request.args.to_dict()
+    )  # recieved credentials
+    sandboxNum = int(data_from_swagger.pop("sandboxNumber"))
+    print("json data for IAP login>>", data_to_IAP := {"user": data_from_swagger})
 
-    try:
-        login_resp = requests.post(f'http://titansdevcarrier.clcloud.af.qwest.net:40{sandboxNum}/login',json=data_to_IAP)
-        if login_resp.status_code == 200:
-            print('recieved token>>', token := login_resp.text)
-            return Response(token, status=200)
-        elif login_resp.status_code == 500:   #can be MongoServerSelectionError or invalid username/password
-            return Response(login_resp.json()['error'],status=400)  #will return error message
-    except:
-        pass
-    return Response("Can't reach the IAP server",status=400)
+    return get_token_response(sandboxNum, data_to_IAP)
 
-app.run(use_reloader=True, debug=False)
+
+@app.route("/start_wf", methods=["POST"])
+def start_workflow():
+    print("URL parameters from swagger>>", url_parameters := request.args.to_dict())
+    sandboxNum, wf_name = (
+        int(url_parameters["sandboxNumber"]),
+        url_parameters["workflowName"],
+    )
+    print(
+        "payload recieved from swagger>>",
+        json.dumps(payload_from_swagger := request.json, indent=4),
+    )
+
+    return execute_wf(sandboxNum, wf_name, payload_from_swagger)
+
+
+app.run(use_reloader=True, debug=True)
---
+++ sandbox_apis.py
@@ -0,0 +1,44 @@
+from flask import Response
+import requests
+
+
+def get_token_response(sandboxNumber: int, credentials: dict) -> tuple:
+    url = f"http://titansdevcarrier.clcloud.af.qwest.net:40{sandboxNumber}/login"
+    try:
+        login_resp = requests.post(url, json=credentials)
+        if login_resp.status_code == 200:
+            print("recieved token>>", token := login_resp.text)
+            return token, 200
+        elif login_resp.status_code == 500:  # can be MongoServerSelectionError or invalid username/password
+            return login_resp.json()["error"], 400  # will return error message
+    except:
+        pass
+    return "Can't reach the IAP server", 400
+
+
+def execute_wf(sandboxNumber: int, wf_name: str, payload: dict) -> tuple:
+    cred = {"user": {"username": "admin@pronghorn","password": "admin"}}
+    token_resp = get_token_response(sandboxNumber, cred)
+    token: str = token_resp[0]  # either a valid token or an error message
+
+    if token_resp[1] == 200:  # in case of a valid token
+        start_wf_url = f"http://titansdevcarrier.clcloud.af.qwest.net:40{sandboxNumber}/workflow_engine/startJobWithOptions/{wf_name}?token={token}"
+        try:
+            req_body = {
+                "options": {"description": "", "variables": payload},
+                "groups": [],
+                "type": "automation",
+            }
+            execute_wf_resp = requests.post(start_wf_url, json=req_body)
+            if execute_wf_resp.status_code == 200 and "application/json" in execute_wf_resp.headers["content-type"]:
+                resp_data: dict = execute_wf_resp.json()
+                job_id = str(resp_data["_id"])
+                job_url = f"http://titansdevcarrier.clcloud.af.qwest.net:40{sandboxNumber}/workflow_engine/viewer?job_id={job_id}"
+                return {"job_id": job_id, "job_url": job_url}, 200
+            else:
+                print("execute WF resp from IAP>>", execute_wf_resp.text)
+                return "Cannot start the workflow", 400
+        except:
+            return "Cannot start the workflow", 400
+    else:
+        return token_resp  # it will have an error message
